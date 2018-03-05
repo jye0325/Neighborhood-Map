5012021ca96d1828c794fd75fdbf3ab89f016f32
Jackie Ye <jackieye@Jackies-MBP-2.fios-router.home>
2018-03-05 13:24:30 -0500

    v0.1.0 Initial Commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// Initial parameters 
+
+// ViewModel - Defines the data and behavior of my UI
+var ViewModel = function () {
+    var self = this;
+}
+
+// Model - Defines the data for the ViewModel
+
+ko.applyBindings(new ViewModel())
